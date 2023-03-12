561726d9fde009607a6791e575d6d7f4202db2c1
jorgeAgoiz <jorgeagoiz@gmail.com>
2023-03-12 20:16:03 +0100

    getStaticPaths & getStaticProps changed by SSR

--- src/pages/profiles/[username]/index.tsx
+++ src/pages/profiles/[username]/index.tsx
@@ -3,13 +3,13 @@ import Layout from "@/components/layout";
 import ProfileDetailsCard from "@/components/profile-details-card";
 import { COMPANY_NAME, ENDPOINT_SEED } from "@/config/constants";
 import { getProfiles } from "@/services/getProfiles";
-import { Profile, Response } from "@/types/profile";
+import styles from "@/styles/details-page.module.css";
+import { Profile } from "@/types/profile";
 import { addressParser } from "@/utils/addressParser";
 import { fullnameParser } from "@/utils/fullnameParser";
 import { Params } from "next/dist/shared/lib/router/utils/route-matcher";
 import Head from "next/head";
 import { NextRouter, useRouter } from "next/router";
-import styles from "./profile-details.module.css";
 
 interface Props {
   profile: Profile;
@@ -22,7 +22,7 @@ const ProfileDetails = ({ profile }: Props): JSX.Element => {
   return (
     <>
       <Head>
-        <title>Profile Details - {COMPANY_NAME}</title>
+        <title>{`Profile Details - ${COMPANY_NAME}`}</title>
         <meta
           name="description"
           content={`Information about ${profile.name.first} profile`}
@@ -59,25 +59,7 @@ const ProfileDetails = ({ profile }: Props): JSX.Element => {
   );
 };
 
-export const getStaticPaths = async () => {
-  const profiles: Response = await getProfiles({
-    amount: 50,
-    seed: ENDPOINT_SEED,
-  });
-  const paths = profiles.results.map((profile) => {
-    return {
-      params: {
-        username: profile.name.first.replaceAll(" ", "-"),
-      },
-    };
-  });
-  return {
-    paths,
-    fallback: false,
-  };
-};
-
-export const getStaticProps = async ({ params }: { params: Params }) => {
+export const getServerSideProps = async ({ params }: { params: Params }) => {
   const { username } = params;
   const profiles = await getProfiles({ amount: 50, seed: ENDPOINT_SEED });
   const selectedProfile = profiles.results.find((profile) => {
